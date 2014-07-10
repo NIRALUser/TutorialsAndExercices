7f050c968e19c922bc3e02d8acfd7917680ee0b0
jeanyves <jean-yves.yang@cpe.fr>
2014-07-10 16:41:33 -0400

    Last changes for the threshold project, tests upcoming soon.

--- Exercices/Threshold/C++/Threshold.cxx
+++ Exercices/Threshold/C++/Threshold.cxx
@@ -5,33 +5,38 @@
 #include "ThresholdCLP.h"
 
 int main(int argc, char *argv[])
-{	
-	PARSE_ARGS;	
- 
-	//Image Type//
-	typedef itk::Image<unsigned char, 3> ImageType;
+{  
+  PARSE_ARGS ;
+  if( threshold > 255 || threshold < 0 )
+  {
+    std::cerr << "Threshold has to be between 0 and 255" << std::endl ;
+    return EXIT_FAILURE ;
+  }
+  //Image Type//
+  typedef itk::Image<unsigned char, 3> ImageType;
 
-	//Input Image//
-	typedef itk::ImageFileReader<ImageType> ReaderType; 
-	ReaderType::Pointer reader = ReaderType::New(); 
-	reader->SetFileName(input); 
+  //Input Image//
+  typedef itk::ImageFileReader<ImageType> ReaderType; 
+  ReaderType::Pointer reader = ReaderType::New(); 
+  reader->SetFileName(input); 
 
-	//Threshold Filtering//
-	typedef itk::BinaryThresholdImageFilter <ImageType, ImageType> FilterType;
-	FilterType::Pointer thresholdFilter=FilterType::New(); 
-	
-	thresholdFilter->SetInput(reader->GetOutput());
+  //Threshold Filtering//
+  typedef itk::BinaryThresholdImageFilter <ImageType, ImageType> FilterType;
+  FilterType::Pointer thresholdFilter=FilterType::New(); 
+  
+  thresholdFilter->SetInput(reader->GetOutput());
 
-	thresholdFilter->SetLowerThreshold(threshold);
-	thresholdFilter->SetUpperThreshold(255);
+  thresholdFilter->SetLowerThreshold(threshold);
+  thresholdFilter->SetUpperThreshold(255);
 
-	thresholdFilter->SetOutsideValue(0); 
-	thresholdFilter->SetInsideValue(255); 
-	
-	//Output Image
-	typedef itk::ImageFileWriter<ImageType> WriterType;  
-	WriterType::Pointer writer = WriterType::New(); 
-	writer->SetInput(thresholdFilter->GetOutput()); 
-	writer->SetFileName(output);
-	writer->Update(); 
+  thresholdFilter->SetOutsideValue(0); 
+  thresholdFilter->SetInsideValue(255); 
+  
+  //Output Image
+  typedef itk::ImageFileWriter<ImageType> WriterType;  
+  WriterType::Pointer writer = WriterType::New(); 
+  writer->SetInput(thresholdFilter->GetOutput()); 
+  writer->SetFileName(output);
+  writer->Update(); 
+  return 0;
 }
